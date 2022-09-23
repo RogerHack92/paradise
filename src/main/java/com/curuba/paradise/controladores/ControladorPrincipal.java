507c6b490495b0ef550f02a9657d25500ba474b5
RogerHack92 <108960587+RogerHack92@users.noreply.github.com>
2022-09-22 19:30:13 -0500

    inicio

--- src/main/java/com/curuba/paradise/controladores/ControladorPrincipal.java
+++ src/main/java/com/curuba/paradise/controladores/ControladorPrincipal.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.curuba.paradise.controladores;
 
-/**
- *
- * @author contabilidad2
- */
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ControladorPrincipal {
-    
+    public String cargarIndex(){
+        return "index";
+        }
 }
+    
