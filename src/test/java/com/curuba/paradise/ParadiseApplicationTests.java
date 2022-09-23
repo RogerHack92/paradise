57c9e1cb033eae07afe8fba383995a79a4a4b9bd
RogerHack92 <108960587+RogerHack92@users.noreply.github.com>
2022-09-22 19:00:35 -0500

    Initial commit

---
+++ src/test/java/com/curuba/paradise/ParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curuba.paradise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
